be97581fe945d052fa1968c514c8ca0b4840c56c
Aurbcd <47425110+Aurbcd@users.noreply.github.com>
2020-01-08 20:02:59 +0100

    Caméra améliorée
    Condition de caméra a faire et gérer la double rotation

--- Assets/KeyOfMagic/CameraRotation.cs
+++ Assets/KeyOfMagic/CameraRotation.cs
@@ -6,17 +6,13 @@ public class CameraRotation : MonoBehaviour
 {
 
     public Transform PlayerTransform;
-
     private Vector3 _cameraOffset;
-
     [Range(0.01f, 1.0f)]
     public float SmoothFactor = 0.5f;
-
     public bool LookAtPlayer = false;
-
     public bool RotateAroundPlayer = true;
-
     public float RotationsSpeed = 5.0f;
+    public float fieldOfView = 60f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,24 +24,35 @@ public class CameraRotation : MonoBehaviour
     void LateUpdate()
     {
 
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+        Debug.Log(Vector3.Angle(_cameraOffset, new Vector3 (0,1,0)));
+        Debug.Log(Input.GetAxis("Mouse X"));
+        Debug.Log(Vector3.Angle(_cameraOffset, new Vector3(0, 1, 0)) > 10 && Input.GetAxis("Mouse Y") > 0);
 
-
-            Vector3 newPos = PlayerTransform.position + _cameraOffset;
-
-            transform.position = Vector3.Lerp(transform.position, newPos, SmoothFactor);
+        transform.position = Vector3.Lerp(transform.position, newPos, SmoothFactor);
         if (Input.GetMouseButton(1))
         {
-            if (RotateAroundPlayer)
-            {
-                Quaternion camTurnAngle =
-                    Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationsSpeed, Vector3.up);
-                _cameraOffset = camTurnAngle * _cameraOffset;
+            //if (Vector3.Angle(_cameraOffset, new Vector3(0, 1, 0)) > 10 || Input.GetAxis("Mouse Y")<0 )
+            //{
+                if (RotateAroundPlayer)
+                {
+                    Quaternion camTurnAngleY =
+                        Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationsSpeed, Vector3.up);
 
-            }
-            if (LookAtPlayer || RotateAroundPlayer)
+                    Quaternion camTurnAngleX =
+                        Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * RotationsSpeed, Vector3.left);
+
+                    _cameraOffset = camTurnAngleX * _cameraOffset;
+
+                }
+            //}
+        }
+        if (LookAtPlayer || RotateAroundPlayer)
             {
                 transform.LookAt(PlayerTransform);
             }
-        }
+        
+        // Pour zoom : Input.mouseScrollDelta.y * 5f;
+
     }
 }
