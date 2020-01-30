3b0821e977ceb0fe802bfb2376242b5bb9a91ead
LemonIceStuff <43790892+LemonIceStuff@users.noreply.github.com>
2020-01-30 12:53:00 +0100

    Revert "Saut de Gauthier"
    This reverts commit 4fb9779e1e38dfc3984d8e333e24af5889b3dae5.

--- Assets/KeyOfMagic/ClickToMove.cs
+++ Assets/KeyOfMagic/ClickToMove.cs
@@ -6,14 +6,9 @@ using UnityEngine.AI;
 public class ClickToMove : MonoBehaviour
 {
     private Animator mAnimator;
-    
+
     private NavMeshAgent mNavMeshAgent;
     private bool mRunning = false;
-    private bool isGrounded = true;
-    public float jumpForce = 5f;
-    private Vector3 jump;
-    private Vector3 velocity;
-    private Vector3 destination;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +16,7 @@ public class ClickToMove : MonoBehaviour
 
         mAnimator = GetComponent<Animator>();
         mNavMeshAgent = GetComponent<NavMeshAgent>();
-        jump = new Vector3(0.0f, 2.0f, 0.0f);
+
 
     }
 
@@ -32,46 +27,22 @@ public class ClickToMove : MonoBehaviour
 
         RaycastHit hit;
 
-        if(Input.GetMouseButton(0) && isGrounded)
+        if(Input.GetMouseButton(0))
         {
             if(Physics.Raycast(ray, out hit, 100))
             {
-                destination = hit.point;
-                mNavMeshAgent.destination = destination;
+                mNavMeshAgent.destination = hit.point;
             }
         }
-
-
-
-        if ((mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance)   || ((Mathf.Abs(destination.x - gameObject.transform.position.x)) < 0.2f && (Mathf.Abs(destination.z - gameObject.transform.position.z)) < 0.2f))
+        if(mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance)
         {
             mRunning = false;
-            mNavMeshAgent.ResetPath();
         }
         else
         {
             mRunning = true;
         }
-        
 
         mAnimator.SetBool("Moving", mRunning);
-
-        if (Input.GetKeyDown("space") && isGrounded)
-        {
-            velocity = new Vector3(0, 0, 0);
-            velocity += mNavMeshAgent.velocity;
-            mNavMeshAgent.enabled = false;
-            GetComponent<Rigidbody>().velocity = velocity * 1.3f;
-            GetComponent<Rigidbody>().AddForce(0, 100 * jumpForce, 0, ForceMode.Impulse);
-            isGrounded = false;
-        }
-
-
-    }
-
-    void OnCollisionStay()
-    {
-        isGrounded = true;
-        mNavMeshAgent.enabled = true;
     }
 }
