89ed2c643b68ff54da7c9fb84138ec067ef03f21
Aurbcd <47425110+Aurbcd@users.noreply.github.com>
2020-02-20 13:16:36 +0100

    Collab de Gauthier -> Ennemi aggro
    Monstre représenté par un cube qui est aggro et se déplace en conséquence

---
+++ Assets/KeyOfMagic/Scripts/ClickToMove.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour
+{
+    private Animator mAnimator;
+    
+    private NavMeshAgent mNavMeshAgent;
+    private bool mRunning = false;
+    private bool isGrounded = true;
+    public float jumpForce = 5f;
+    private Vector3 jump;
+    private Vector3 velocity;
+    private Vector3 destination;
+
+    public static Vector3 playerPosition;
+    public bool combatMode = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mAnimator = GetComponent<Animator>();
+        mNavMeshAgent = GetComponent<NavMeshAgent>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = GetComponent<Transform>().position;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if(Input.GetMouseButton(0))
+        {
+            if(Physics.Raycast(ray, out hit, 100) )
+            {
+                destination = hit.point;
+                mNavMeshAgent.destination = destination;
+            }
+        }
+
+
+        
+        if (mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance)
+        {
+            mRunning = false;
+            //mNavMeshAgent.ResetPath();
+        }
+        else
+        {
+            mRunning = true;
+        }
+        
+
+        mAnimator.SetBool("Moving", mRunning);
+
+        /*
+        if (Input.GetKeyDown("space") && isGrounded)
+        {
+            velocity = new Vector3(0, 0, 0);
+            velocity += mNavMeshAgent.velocity;
+            mNavMeshAgent.enabled = false;
+            GetComponent<Rigidbody>().velocity = velocity * 1.3f;
+            GetComponent<Rigidbody>().AddForce(0, 100 * jumpForce, 0, ForceMode.Impulse);
+            isGrounded = false;
+        }
+        */
+
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+        mNavMeshAgent.enabled = true;
+    }
+}
---
+++ Assets/KeyOfMagic/Scripts/MonsterScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterScript : MonoBehaviour
+{
+    public int healthPoints;
+    public string monsterName;
+
+    private NavMeshAgent mNavMeshAgent;
+    private bool mRunning = false;
+    private bool isGrounded = true;
+
+    private float distanceToPlayer;
+
+    void Start()
+    {
+        mNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = (GetComponent<Transform>().position - ClickToMove.playerPosition).magnitude;
+
+        if (mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance)
+        {
+            StopMovement();
+        }
+
+        if (distanceToPlayer < 10)
+        {
+            MoveInDirection(ClickToMove.playerPosition);
+        }
+        
+        if (distanceToPlayer < 5)
+        {
+            StopMovement();
+        }
+
+    }
+
+    public void MoveInDirection(Vector3 direction)
+    {
+        mNavMeshAgent.destination = direction;
+    }
+
+    public void StopMovement()
+    {
+        mNavMeshAgent.ResetPath();
+    }
+}
