f3686af4995d6afbf229962fc8e1edcb420dcc92
LemonIceStuff <43790892+LemonIceStuff@users.noreply.github.com>
2020-02-21 20:36:40 +0100

    Ajout d'une barre de vie pseudo fonctionnelle pour les monstres
    C'est un canvas chelou avec un billboard bizarre

--- Assets/KeyOfMagic/Scripts/MonsterStatText.cs
+++ Assets/KeyOfMagic/Scripts/MonsterStatText.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class MonsterStatText : MonoBehaviour
 {
     public string monsterName;
-    public float PV = 150;
-    public float PVMax = 200;
+    public int PV = 150;
+    public int PVMax = 200;
 
     public GameObject TextName;
     public GameObject SpriteSelection;
 
+    public HealthBar BarreDeVie;
+    public GameObject Barre;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        BarreDeVie.SetMaxHealth(PVMax);
+        BarreDeVie.SetHealth(PV);
     }
 
     // Update is called once per frame
@@ -24,18 +29,24 @@ public class MonsterStatText : MonoBehaviour
         {
             TextName.transform.LookAt(Camera.main.transform.position);
             TextName.transform.Rotate(0, 180, 0);
+            BarreDeVie.SetHealth(PV);                                                                                       //Update barre de vie
             TextName.GetComponent<TextMesh>().text = "" + monsterName + " | " + PV;  //EN ATTENDANT UNE BARRE DE VIE
         }
 
-        if (gameObject.GetComponent<MonsterMouvSelection>().estSelectionne)
+        if (gameObject.GetComponent<MonsterMouvSelection>().estSelectionne)                                        // Quand le monstre est sélectionné
         {
-            SpriteSelection.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.75f);
+            SpriteSelection.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.75f); //Aligner sprite
             SpriteSelection.transform.LookAt(Camera.main.transform.position);
             SpriteSelection.transform.Rotate(0, 180, 0);
+
+            //Barre.GetComponent<CanvasRenderer>().SetAlpha(0.5f);                                                                      // Aligner Barre
+            Barre.transform.LookAt(Camera.main.transform.position);
+            Barre.transform.Rotate(0, 180, 0);
         }
         if(gameObject.GetComponent<MonsterMouvSelection>().estSelectionne == false)
         {
             SpriteSelection.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0f);
+            //Barre.GetComponent<CanvasRenderer>().SetAlpha(0f);
         }
     }
 }
