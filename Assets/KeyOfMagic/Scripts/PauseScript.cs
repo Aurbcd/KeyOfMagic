68dd87f3208e39c7feb610450b9030c962fd73aa
LemonIceStuff <43790892+LemonIceStuff@users.noreply.github.com>
2020-03-02 15:43:21 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/Aurbcd/KeyOfMagic""
    This reverts commit 71a0c1a2330e3db607d4f3ae8c77670ea2400c18.

---
+++ Assets/KeyOfMagic/Scripts/PauseScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        GameIsPaused = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        AudioListener.volume = 1f;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        AudioListener.volume = 0.3f;
+    }
+
+    public void LoadMenu()
+    {
+        GameIsPaused = false;
+        Debug.Log("Loading Menu...");
+        Time.timeScale = 1f;
+        AudioListener.volume = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Retry()
+    {
+        GameIsPaused = false;
+        pauseMenuUI.SetActive(false);
+        Debug.Log("Launching Game");
+        Time.timeScale = 1f;
+        AudioListener.volume = 1f;
+        SceneManager.LoadScene("Game");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+    
+}
