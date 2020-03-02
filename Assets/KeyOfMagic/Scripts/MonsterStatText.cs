0042c31c1f6c8bab6b8e0e68a532c693b3f70cf2
Aurbcd <47425110+Aurbcd@users.noreply.github.com>
2020-03-02 16:00:43 +0100

    Réparation partielle
    On fait ce qu'on peut

--- Assets/KeyOfMagic/Scripts/MonsterStatText.cs
+++ Assets/KeyOfMagic/Scripts/MonsterStatText.cs
@@ -5,22 +5,16 @@ using UnityEngine;
 public class MonsterStatText : MonoBehaviour
 {
     public string monsterName;
-    public int PV = 150;
-    public int PVMax = 200;
+    public float PV = 150;
+    public float PVMax = 200;
 
     public GameObject TextName;
     public GameObject SpriteSelection;
 
-    public HealthBar BarreDeVie;
-    private CanvasGroup Barre;
-
-
     // Start is called before the first frame update
     void Start()
     {
-        BarreDeVie.SetMaxHealth(PVMax);
-        BarreDeVie.SetHealth(PV);
-        Barre = BarreDeVie.GetComponent<CanvasGroup>();
+        
     }
 
     // Update is called once per frame
@@ -30,27 +24,18 @@ public class MonsterStatText : MonoBehaviour
         {
             TextName.transform.LookAt(Camera.main.transform.position);
             TextName.transform.Rotate(0, 180, 0);
-            BarreDeVie.SetHealth(PV);                                                                                       //Update barre de vie
             TextName.GetComponent<TextMesh>().text = "" + monsterName + " | " + PV;  //EN ATTENDANT UNE BARRE DE VIE
         }
 
-        if (gameObject.GetComponent<MonsterMouvSelection>().estSelectionne)                                        // Quand le monstre est sélectionné
+        if (gameObject.GetComponent<MonsterMouvSelection>().estSelectionne)
         {
-            SpriteSelection.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.75f); //Aligner sprite
+            SpriteSelection.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.75f);
             SpriteSelection.transform.LookAt(Camera.main.transform.position);
             SpriteSelection.transform.Rotate(0, 180, 0);
-
-            //Barre.GetComponent<CanvasRenderer>().SetAlpha(0.5f);                                                                      // Aligner Barre
-
-            Barre.alpha = 0.5f;
-/*             Barre.transform.LookAt(Camera.main.transform.position);
-            Barre.transform.Rotate(0, 180, 0); */
         }
         if(gameObject.GetComponent<MonsterMouvSelection>().estSelectionne == false)
         {
             SpriteSelection.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0f);
-            //Barre.GetComponent<CanvasRenderer>().SetAlpha(0f);
-            Barre.alpha = 0f;
         }
     }
 }
