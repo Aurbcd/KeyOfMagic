96ba8b46e25422cc6052d8b853a1b9980c1cacf5
LemonIceStuff <43790892+LemonIceStuff@users.noreply.github.com>
2020-03-08 18:26:45 +0100

    Nouvelle UI du monstre
    Barre de vie multicolor fonctionelle, nouveau texte (TextUI) dans un canvas

--- Assets/KeyOfMagic/Scripts/MonsterStatText.cs
+++ Assets/KeyOfMagic/Scripts/MonsterStatText.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MonsterStatText : MonoBehaviour
 {
@@ -9,9 +10,10 @@ public class MonsterStatText : MonoBehaviour
     public int PVMax = 200;
 
     public HealthBar Barre;
+
     public CanvasGroup CanvasGBarre;
-    public GameObject TextName;
-    public GameObject SpriteSelection;
+
+    public Text Text;
 
     // Start is called before the first frame update
     void Start()
@@ -23,25 +25,19 @@ public class MonsterStatText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (TextName != null)
-        {
-            TextName.transform.LookAt(Camera.main.transform.position);
-            TextName.transform.Rotate(0, 180, 0);
-            TextName.GetComponent<TextMesh>().text = "" + monsterName + " | " + PV;  //EN ATTENDANT UNE BARRE DE VIE
-            Barre.SetHealth(PV);
-        }
+        Text.transform.LookAt(Camera.main.transform.position);
+        Text.transform.Rotate(0, 180, 0);
+        Text.text = "" + monsterName;
+        Barre.SetHealth(PV);
+        Barre.fill.color =  Barre.gradient.Evaluate(Barre.slider.normalizedValue);
+
 
         if (gameObject.GetComponent<MonsterMouvSelection>().estSelectionne)
         {
-            SpriteSelection.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.75f);
-            SpriteSelection.transform.LookAt(Camera.main.transform.position);
-            SpriteSelection.transform.Rotate(0, 180, 0);
-
             CanvasGBarre.alpha = 0.75f;
         }
         if(gameObject.GetComponent<MonsterMouvSelection>().estSelectionne == false)
         {
-            SpriteSelection.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0f);
             CanvasGBarre.alpha = 0f;
         }
     }
