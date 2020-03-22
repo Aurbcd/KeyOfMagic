209940ac710e2b14c75f13696a58f8968bf301ad
LemonIceStuff <43790892+LemonIceStuff@users.noreply.github.com>
2020-03-22 22:08:03 +0100

    Started range display

---
+++ Assets/KeyOfMagic/Scripts/RangeDisplay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+[RequiredComponent(typeof(LineRenderer))]
+public class RangeDisplay : MonoBehaviour
+{
+        public int vertexCount = 40; //The more, the smoother
+        public float lineWidth = 0.2f;
+        public float radius;
+
+
+        private void Awake() 
+        {
+            lineRenderer = GetComponent
+        }
+}
