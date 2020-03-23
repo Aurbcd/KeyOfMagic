a555824cd61606d1426621dbceccf58dfa2b2018
LemonIceStuff <43790892+LemonIceStuff@users.noreply.github.com>
2020-03-23 22:43:42 +0100

    Stabilized projector and better spellbook artwork

---
+++ Assets/KeyOfMagic/Scripts/ProjectorFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectorFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+
+}
---
+++ Assets/KeyOfMagic/Scripts/RotatingImage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingImage : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.Rotate(0, 0, 50 * Time.deltaTime);        
+    }
+}
