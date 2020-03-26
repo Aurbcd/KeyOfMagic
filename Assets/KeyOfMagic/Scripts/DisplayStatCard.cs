1507990e75968f171397e7230b99e4936ee2ff3d
LemonIceStuff <43790892+LemonIceStuff@users.noreply.github.com>
2020-03-26 23:52:08 +0100

    Début d'implémentation du pannel OnMouseOver

---
+++ Assets/KeyOfMagic/Scripts/DisplayStatCard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DisplayStatCard : MonoBehaviour
+{
+
+    public CanvasGroup canvasGroup;
+    private bool isDisplayed;
+    public float fadeSpeed;
+    void Start()
+    {
+        canvasGroup.alpha = 0f;
+    }
+
+    void Update()
+    {
+        if(isDisplayed)
+        {
+            canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 1f, fadeSpeed * Time.deltaTime);
+        }
+        else
+        {
+            canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 0f, fadeSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnMouseOver()
+    {
+        isDisplayed = true;
+    }
+
+    void OnMouseExit()
+    {
+        isDisplayed = false;
+    }
+    
+
+}
