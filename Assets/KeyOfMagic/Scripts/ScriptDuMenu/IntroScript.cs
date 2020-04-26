d6e66b0918510e4187a20bc1cd929fdb28dee015
Aurbcd <47425110+Aurbcd@users.noreply.github.com>
2020-04-26 02:00:59 +0200

    Tutoriel terminé

---
+++ Assets/KeyOfMagic/Scripts/ScriptDuMenu/IntroScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class IntroScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Tutoriel());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("Start Menu");
+        } 
+    }
+
+    IEnumerator Tutoriel()
+    {
+        yield return new WaitForSeconds(13);
+        SceneManager.LoadScene("Tutoriel");
+    }
+}
