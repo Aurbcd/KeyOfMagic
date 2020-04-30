85659ccc8323f43a4f553571e912b26a84f61c7a
Aurbcd <47425110+Aurbcd@users.noreply.github.com>
2020-05-01 00:47:33 +0200

    Belles choses

--- Assets/KeyOfMagic/Scripts/CameraRotationFixed.cs
+++ Assets/KeyOfMagic/Scripts/CameraRotationFixed.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class CameraRotationFixed : MonoBehaviour
 {
 
-    public float turnSpeedX = 7.0f;
-    public float turnSpeedY = 5.0f;
+    public static float turnSpeed = 5.0f;
 
     public bool touch;
     public Transform player;
@@ -24,10 +23,10 @@ public class CameraRotationFixed : MonoBehaviour
         vectorRotation = player.position - gameObject.GetComponent<Transform>().position;
         if (Input.GetMouseButton(1))
         {
-            offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeedX, Vector3.up) * offset;
+            offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
             if ((Input.GetAxis("Mouse Y") < 0 && Vector3.Angle(Vector3.up, vectorRotation) < 140) || (Input.GetAxis("Mouse Y") > 0 && Vector3.Angle(Vector3.up, vectorRotation) > 100))
             {
-                offset = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeedY, Vector3.Cross(vectorRotation, Vector3.up)) * offset;
+                offset = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.Cross(vectorRotation, Vector3.up)) * offset;
             }
         }
         playerHeadPosition = new Vector3(player.position.x, player.position.y +2, player.position.z);
---
+++ Assets/KeyOfMagic/Scripts/ScriptDuMenu/MenuOption.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Audio;
+
+public class MenuOption : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void SetVolume(float vol)
+    {
+        audioMixer.SetFloat("VolMaster", vol);
+    }
+
+    public void SetSensibility(float sens)
+    {
+        CameraRotationFixed.turnSpeed = sens;
+    }
+
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    public void SetMusic(bool m)
+    {
+        if (m)
+        {
+            audioMixer.SetFloat("VolMusic", -5);
+        }
+        if (!m)
+        {
+            audioMixer.SetFloat("VolMusic", -80);
+        }
+    }
+    public void SetNotif(bool n)
+    {
+        if (n)
+        {
+            audioMixer.SetFloat("VolNotif", -5);
+        }
+        if (!n)
+        {
+            audioMixer.SetFloat("VolNotifs", -80);
+        }
+    }
+}
